5088f7bca15646a7045c130a438c741e59a796a4
Joshua <joshua@syntacticsinc.com>
2017-06-22 15:56:09 +0800

    start spa

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -8,15 +8,52 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+window.VueRouter = require('vue-router');
+window.NProgress = require('nprogress');
+Vue.use(VueRouter);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
+const App = require('./views/App.vue');
 
-Vue.component('example', require('./components/Example.vue'));
+const routes = [];
+
+Vue.http.interceptors.push((request, next) => {
+	NProgress.start();
+	const authToken = auth.getToken();
+    request.headers.set('Authorization', `Bearer ${authToken}`);
+
+    next((response) => {
+    	NProgress.done();
+    	if (response.status == 404) {
+    	  router.go('/');
+    	} else if (response.status == 401 && response.data.refreshed_token) {
+    	  auth.setToken(response.data.refreshed_token);
+    	} else if (response.data.token_expired){
+    		router.replace('/logout');
+    	}
+    });
+});
+const router = new VueRouter({
+	routes,
+	mode: 'history'
+});
+
+router.beforeEach((to, from, next) => {
+	NProgress.start();
+	next();
+});
+
+router.afterEach((to, from) => {
+	sessionStorage.setItem('previousLink', from.fullPath);
+	NProgress.done();
+});
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+	router,
+	render: h => h(App)
 });
---
+++ resources/assets/js/views/App.vue
@@ -0,0 +1,16 @@
+<script>
+	export default {
+		created(){
+
+		},
+		data(){
+			return {};
+		}
+	}
+</script>
+
+<template>
+	<div id="app">
+		
+	</div>
+</template>
